22cee6cb1526c1333b9f4e1b6401340d38a79cf6
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-04-15 09:12:32 +0200

    Added Reception controller

---
+++ Morgenmadsbuffet/Controllers/ReceptionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Morgenmadsbuffet.Controllers
+{
+    public class ReceptionController : Controller
+    {
+
+        [Authorize("IsReceptionist")]
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Morgenmadsbuffet/Models/Rooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morgenmadsbuffet.Models
+{
+    public class Rooms
+    {
+        [Key]
+        public int RoomNumber { get; set; }
+
+        public int AmountChildren { get; set; }
+
+        public int AmountAdults { get; set; }
+
+
+
+
+    }
+}
