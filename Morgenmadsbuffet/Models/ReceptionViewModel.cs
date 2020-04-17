6b0ce08900e98e50566839f5de9a1560af8ec7ce
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-17 14:49:01 +0200

    new way of life

---
+++ Morgenmadsbuffet/Models/ReceptionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Morgenmadsbuffet.Models
+{
+    public class ReceptionViewModel
+    {
+        public List<Bookings> bookings { get; set; }
+
+        public int totalAmountofGuests()
+        {
+
+        }
+
+    }
+}
