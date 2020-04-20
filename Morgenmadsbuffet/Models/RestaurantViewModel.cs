ff4f9fe1fbabcb3ac9c452b4f1bdbc3874e7d145
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-04-20 11:07:06 +0200

    View for waiter/restaurant is now good

--- Morgenmadsbuffet/Models/RestaurantViewModel.cs
+++ Morgenmadsbuffet/Models/RestaurantViewModel.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Morgenmadsbuffet.Models
 {
     
     public class RestaurantViewModel
     {
-        public List<Bookings> Bookings { get; set; }
+        public List<Bookings> Bookings { get; set; } = new List<Bookings>();
 
-        //public int TotalAmountGuests { get; set; }
-        //public int RoomNumber { get; set; }
-        //public int AmountAdults { get; set; }
-        //public int AmountChildren { get; set; }
-
-        public bool Checkedin
+        public int TotalAmountGuests
         {
-            get;
-            set;
+            get
+            {
+                var total = 0;
+                for (int i = 0; i < Bookings.Count(); i++)
+                {
+                    total = Bookings[i].AmountAdults + Bookings[i].AmountChildren;
+                }
+
+                return total;
+            } 
+            set
+            {
+
+            }
         }
     }
 }
